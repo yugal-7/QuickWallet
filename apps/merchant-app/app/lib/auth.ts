0fbdc696ce5f4532b3405ea6f097033d83d0b3cc
Yugal Vashishth <yugalvashishth@Yugals-Laptop.local>
2024-07-20 19:48:12 +0530

    added transactions table/schema

--- apps/merchant-app/app/lib/auth.ts
+++ apps/merchant-app/app/lib/auth.ts
@@ -1,4 +1,5 @@
 import GoogleProvider from "next-auth/providers/google";
+import db from "@repo/db/client";
 
 export const authOptions = {
     providers: [
@@ -7,4 +8,41 @@ export const authOptions = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
         })
     ],
+    callbacks: {
+      async signIn({ user, account }: {
+        user: {
+          email: string;
+          name: string
+        },
+        account: {
+          provider: "google" | "github"
+        }
+      }) {
+        console.log("hi signin")
+        if (!user || !user.email) {
+          return false;
+        }
+
+        await db.merchant.upsert({
+          select: {
+            id: true
+          },
+          where: {
+            email: user.email
+          },
+          create: {
+            email: user.email,
+            name: user.name,
+            auth_type: account.provider === "google" ? "Google" : "Github" // Use a prisma type here
+          },
+          update: {
+            name: user.name,
+            auth_type: account.provider === "google" ? "Google" : "Github" // Use a prisma type here
+          }
+        });
+
+        return true;
+      }
+    },
+    secret: process.env.NEXTAUTH_SECRET || "secret"
   }
